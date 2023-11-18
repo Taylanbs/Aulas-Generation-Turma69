7aecc5bb26e583935963ea1f6fcbdb0794c1d33c
Taylanbs <taylanmf@gmail.com>
2023-11-18 11:27:27 -0300

    Aplicação Teste

--- ProjetoGuia/projetoGuia/src/conta/model/Conta.java
+++ ProjetoGuia/projetoGuia/src/conta/model/Conta.java
@@ -1,10 +1,14 @@
 package conta.model;
 
+import java.util.Scanner;
+
 public class Conta {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
-
-	}
-
-}
+		System.out.println("Este é um teste para o inicio do Projeto Guia");
+		
+		   		    
+	
+	
+ }
+}    
